6063b96b59ee909098d6a654c26a198dc62304da
michrog <rogal50112@gmail.com>
2024-09-08 16:51:35 +0200

    added fontawesome, contact info to serious section

--- src/components/SeriousSection.jsx
+++ src/components/SeriousSection.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import '../styles/SeriousSection.css'
 
 export default function SeriousSection(){
     return (
-        <div>This is a <b>serious</b> section.</div>
+        <>
+            <h1>Michał Rogaliński</h1>
+            <ul className="contactList">
+                <li>Email: michal.rogalinski.1999@gmail.com</li>
+                <li>Phone: +48 600 127 558</li>
+                <li>GitHub: <a target="__blank" href="https://github.com/Michrog">https://github.com/Michrog</a></li>
+                <li>Linkedin: <a target="__blank" href="https://www.linkedin.com/in/micha%C5%82-rogali%C5%84ski-0a0374218/">https://www.linkedin.com/in/michał-rogaliński</a></li>
+            </ul>
+
+        </>
     )
 }
